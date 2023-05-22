21994cfe5e68e3d5521464c4234792f67ef8d0ad
Fredrik Nistor <quizz.0n@live.com>
2023-05-22 06:15:53 +0300

    Build

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/orbital-edge-imaging/',
+  base: './',
   plugins: [
     vue({
       template: { transformAssetUrls },
